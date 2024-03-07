6148b391ee9f5e6db4bef7330c65f50cf4209652
jpdacunha <dcjeanpaul@gmail.com>
2024-03-07 15:42:37 +0100

    Registering reactJS app has custom element

--- s3/front-end/app1-docker-example/src/index.js
+++ s3/front-end/app1-docker-example/src/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const ELEMENT_ID = 'app1-docker-example';
+
+class WebComponent extends HTMLElement {
+
+  connectedCallback() {
+		this.root = createRoot(this);
+
+		this.root.render(<App />);
+  }
+
+  disconnectedCallback() {
+
+		//
+		// Unmount React tree to prevent memory leaks.
+		//
+		// See React documentation at
+		//
+		//     https://react.dev/reference/react-dom/client/createRoot#root-unmount
+		//
+		// for more information.
+		//
+
+		this.root.unmount();
+		delete this.root;
+	}
+}
+
+if (!customElements.get(ELEMENT_ID)) {
+  console.log('Registering ' + ELEMENT_ID + ' as custom element');
+	customElements.define(ELEMENT_ID, WebComponent);
+} else {
+  console.log('Skipping registration for ' + ELEMENT_ID + ' (already registered)');
+}
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
